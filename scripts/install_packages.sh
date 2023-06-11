d7fcf16ff65746e47a1e1e3361ac8796e36010f3
5met4nka <pavilich99@mail.ru>
2023-06-11 10:48:18 +0300

    убрал скрипты потому что все собирается из скриптов проектов

--- scripts/install_packages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get install \
-    ros-$ROS_DISTRO-gazebo-plugins \
-    ros-$ROS_DISTRO-ddynamic-reconfigure
-
-#requirements for RS-ros
-sudo apt purge ros-$ROS_DISTRO-librealsense2
