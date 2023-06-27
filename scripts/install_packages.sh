f598d3e7ae6ff8ec84e6146b86aacd77bb070e2c
BlackWidow <blacwidow@etu.com>
2023-06-27 13:06:04 +0300

    new rtabmap version

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -2,7 +2,9 @@
 
 sudo apt-get install \
     ros-$ROS_DISTRO-gazebo-plugins \
-    ros-$ROS_DISTRO-ddynamic-reconfigure
+    ros-$ROS_DISTRO-ddynamic-reconfigure \
+    libpcap-dev
    
 #requirements for RS-ros
 sudo apt purge ros-$ROS_DISTRO-librealsense2
+sudo apt-get install libpcap-dev
