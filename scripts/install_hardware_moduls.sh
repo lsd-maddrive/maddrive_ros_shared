2a3aa00edb59b134239c03e4bb4d5f5d84f3898a
5met4nka <pavilich99@mail.ru>
2023-10-20 10:43:39 +0300

    added checking of ydlidar sdk files before installation

--- scripts/install_hardware_moduls.sh
+++ scripts/install_hardware_moduls.sh
@@ -22,9 +22,17 @@ else
 fi
 
 # YDLidarSDK
-git clone https://github.com/YDLIDAR/YDLidar-SDK.git
-mkdir -p YDLidar-SDK/build
-cd YDLidar-SDK/build
-cmake ..
-make
-sudo make install
+# Путь к файлу ydlidar_sdkConfig.cmake
+SDK_config_file="/usr/local/lib/cmake/ydlidar_sdk/ydlidar_sdkConfig.cmake"
+
+# Проверяем, существует ли файл
+if [ -f "$SDK_config_file" ]; then
+    echo "Файл $SDK_config_file существует. SDK YDLidar уже установлен."
+else
+    git clone https://github.com/YDLIDAR/YDLidar-SDK.git
+    mkdir -p YDLidar-SDK/build
+    cd YDLidar-SDK/build
+    cmake ..
+    make
+    sudo make install
+fi
