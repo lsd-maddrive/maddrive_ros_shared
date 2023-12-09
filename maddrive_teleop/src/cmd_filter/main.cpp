f41febd19ac0707e9c977c72b660a8b4b16d75b7
5met4nka <nlobazev@gmail.com>
2023-12-10 00:02:27 +0300

    added filtering by linear.x message

--- maddrive_teleop/src/cmd_filter/main.cpp
+++ maddrive_teleop/src/cmd_filter/main.cpp
@@ -1,5 +1,3 @@
-
-
 // ROS
 #include <ros/ros.h>
 #include <tf/tf.h>
@@ -11,29 +9,33 @@ class SteeringSmoothFilter{
 
 public:
 
-    SteeringSmoothFilter(ros::NodeHandle &nh, double alpha) : m_alpha(alpha), m_is_initialized(false) {
+    SteeringSmoothFilter(ros::NodeHandle &nh, double alpha_linear, double alpha_angular)
+        : m_alpha_linear(alpha_linear), m_alpha_angular(alpha_angular), m_is_initialized(false) {
+        
         sub_ = nh.subscribe("cmd_vel_raw", 100, &SteeringSmoothFilter::callback, this);
         pub_ = nh.advertise<geometry_msgs::Twist>("cmd_vel", 100);
 
-        ROS_INFO_STREAM("Initialized steering filter with alpha: " << alpha);
+        ROS_INFO_STREAM("Initialized steering filter with alpha_linear: " << alpha_linear << ", alpha_angular: " << alpha_angular);
     }
 
     void callback(const geometry_msgs::Twist::ConstPtr &msg) {
-        double angle_steering = msg->angular.z;
+        double linear_velocity = msg->linear.x;
+        double angular_velocity = msg->angular.z;
 
         if (!m_is_initialized) {
-            m_prev_value = angle_steering;
+            m_prev_linear = linear_velocity;
+            m_prev_angular = angular_velocity;
             m_is_initialized = true;
         }
 
         geometry_msgs::Twist new_msg = *msg;
 
-        double new_steering = m_prev_value*(1-m_alpha) + angle_steering*(m_alpha);
-        
-        // ROS_INFO_STREAM("Raw: " << angle_steering << " / " << new_steering);
+        double new_linear = m_prev_linear * (1 - m_alpha_linear) + linear_velocity * m_alpha_linear;
+        double new_angular = m_prev_angular * (1 - m_alpha_angular) + angular_velocity * m_alpha_angular;
 
-        new_msg.angular.z = m_prev_value = new_steering;
-        
+        new_msg.linear.x = m_prev_linear = new_linear;
+        new_msg.angular.z = m_prev_angular = new_angular;
+
         pub_.publish(new_msg);
     }
 
@@ -42,8 +44,10 @@ private:
     ros::Publisher pub_;
 
     bool m_is_initialized;
-    double m_prev_value;
-    double m_alpha;
+    double m_prev_linear;
+    double m_prev_angular;
+    double m_alpha_linear;
+    double m_alpha_angular;
 };
 
 
@@ -51,10 +55,11 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "steering_filter");
     ros::NodeHandle nh;
 
-    float steering_alpha_param;
+    float steering_alpha_param, linear_alpha_param;
     ros::param::get("~steering_alpha", steering_alpha_param);
+    ros::param::get("~linear_alpha", linear_alpha_param);
 
-    SteeringSmoothFilter filter(nh, steering_alpha_param);
+    SteeringSmoothFilter filter(nh, linear_alpha_param, steering_alpha_param);
 
     ros::spin();
     return 0;
