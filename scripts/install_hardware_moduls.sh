5032815f9997ab408edf1b08f3bd733f1be06c13
5met4nka <nlobazev@gmail.com>
2024-01-09 19:02:06 +0300

    add YAHBOOM IMU installation

--- scripts/install_hardware_moduls.sh
+++ scripts/install_hardware_moduls.sh
@@ -39,3 +39,10 @@ else
     cd ../..
     rm -rf YDLidar-SDK
 fi
+
+# YAHBOOM IMU device
+echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0777", SYMLINK+="imu_usb"' >/etc/udev/rules.d/imu_usb.rules
+
+service udev reload
+sleep 2
+service udev restart
